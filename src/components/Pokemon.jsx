2b254ba1a890c7752a0f92c96d0c15464dd4dbce
chronolai <x941i6c04@gmail.com>
2021-11-06 19:40:40 +0800

    add tolltip

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
+
+import pokemon from "../pokemon";
 
 function Pokemon(props) {
   const { id } = props;
   const padID = id.toString().padStart(3, "0");
-  return <span className={`sprite-icon sprite-icon-${padID}`}></span>;
+
+  const pm = pokemon.find((pm) => pm[0] === padID);
+  return (
+    <Tippy placement="top" content={<div>{`${pm[0]} - ${pm[1]}`}</div>}>
+      <span className={`sprite-icon sprite-icon-${padID}`} />
+    </Tippy>
+  );
 }
 
 export default Pokemon;
